5d661a3f40f2b2b9113c5d2910fb4f3456f8d8a0
yulb1 <kim482185@gmail.com>
2025-11-20 17:55:19 +0900

    기초 패키지 트리

---
+++ src/main/java/org/example/linfinityfirst/LinfinityfirstApplication.java
@@ -0,0 +1,13 @@
+package org.example.linfinityfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinfinityfirstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinfinityfirstApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/linfinityfirst/LinfinityfirstApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.linfinityfirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinfinityfirstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
