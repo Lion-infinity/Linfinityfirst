1d99a612b9523249f2445d93bfe62001928d5014
yulb1 <kim482185@gmail.com>
2025-12-01 17:27:05 +0900

    로그인 부분 삭제 -> Security이용

--- src/main/java/org/example/linfinityfirst/controller/AuthController.java
+++ src/main/java/org/example/linfinityfirst/controller/AuthController.java
@@ -21,14 +21,4 @@ public class AuthController {
         return ResponseEntity.ok("회원가입 성공");
     }
 
-    // 로그인 기능
-    @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginRequestDto requestDto) {
-        try {
-            userService.login(requestDto);
-            return ResponseEntity.ok("로그인 성공");
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
-    }
 }
