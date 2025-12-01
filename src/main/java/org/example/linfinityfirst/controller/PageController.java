a7e87839e2d5770ebab99378567db2f1ecf4f86b
yulb1 <kim482185@gmail.com>
2025-12-01 16:32:32 +0900

    qna 목록, 상세페이지

--- src/main/java/org/example/linfinityfirst/controller/PageController.java
+++ src/main/java/org/example/linfinityfirst/controller/PageController.java
@@ -2,6 +2,7 @@ package org.example.linfinityfirst.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller // 화면(HTML)을 리턴하는 컨트롤러
 public class PageController {
@@ -25,9 +26,20 @@ public class PageController {
     }
 
     // Qna 글쓰기 페이지
-    // SecurityConfig 설정에 의해, 로그인 안 한 사람은 여기 못 들어오고 로그인 페이지로 튕겨짐
     @GetMapping("/qna/write")
     public String qnaWritePage() {
         return "qna/write"; // templates/qna/write.html
+    }
+
+    //Qna 목록 페이지
+    @GetMapping("/qna")
+    public String qnaListPage() {
+        return "qna/list"; // templates/qna/list.html
+    }
+
+    //Qna 상세 페이지
+    @GetMapping("/qna/{id}")
+    public String qnaDetailPage(@PathVariable Long id) {
+        return "qna/detail"; // templates/qna/detail.html
     }
 }
